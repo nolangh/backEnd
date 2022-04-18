37d0265c612286fd06ca87e669e25819338612c5
nolangh <nolan.higgins2245@stu.mtec.edu>
2022-04-18 10:11:54 -0600

    -a

--- april-18/robot.js
+++ april-18/robot.js
@@ -21,3 +21,38 @@ class Robot extends EventEmitter {
 const robot = new Robot("Hank");
 
 robot.emit("speak", "hello");
+
+/* ---------------------- ANCHOR moved this to plant.js --------------------- */
+
+class Plant extends EventEmitter {
+	constructor(size, hasBeenPlanted) {
+		super();
+		this.size = 0;
+		this.hasBeenPlanted = false;
+		this.once("plantSeed", () => {
+			this.size = 1;
+			this.hasBeenPlanted = true
+		});
+		this.on('water', () => {
+			if (this.hasBeenPlanted){
+				this.size ++ 1;
+				console.log(`the plant size is ${this.size}`)
+			}else{
+				console.log('Please plant seed')
+			}
+		});
+		this.on('bugAttack', () => {
+			if(this.hasBeenPlanted){
+				this.size -- 1;
+				console.log(`the PLant size is ${this.size}`) //NOTE change "--" to "-" if it is not incrementing correctly
+			}
+		})
+	}
+}
+
+const plant = new Plant()
+
+plant.emit('plantSeed');
+plant.emit('water');
+plant.emit('harvest')
+plant.emit('bugAttack')
